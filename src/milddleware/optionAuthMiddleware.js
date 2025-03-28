a9ed90bc79d65de59a38264cf58c9df1f094597d
Tung Mai <tungmt@outlook.com>
2025-03-28 16:07:41 +0700

    - update unauthentication request

--- src/milddleware/optionAuthMiddleware.js
+++ src/milddleware/optionAuthMiddleware.js
@@ -8,35 +8,36 @@ function optionAuthMiddleware(req, res, next) {
         const deviceId = req.headers['device-id'];
         const token = authHeader && authHeader.split(' ')[1];
 
-        if (!token)
-            return res
-                .status(401)
-                .json({ message: 'Access denied, no token provided' });
+        if (token) {
+            jwt.verify(token, process.env.JWT_SECRET, async (err, decodedToken) => {
+                if (err) return res.status(403).json({ message: 'Invalid token' });
 
-        jwt.verify(token, process.env.JWT_SECRET, async (err, decodedToken) => {
-            if (err) return res.status(403).json({ message: 'Invalid token' });
-
-            const session = await Sessions.findOne({
-                where: {
-                    user_id: decodedToken.user_id,
-                    id: decodedToken.session_id,
-                    device_id: deviceId ?? '',
-                    logout_date: {
-                        [Op.eq]: null,
+                const session = await Sessions.findOne({
+                    where: {
+                        user_id: decodedToken.user_id,
+                        id: decodedToken.session_id,
+                        device_id: deviceId ?? '',
+                        logout_date: {
+                            [Op.eq]: null,
+                        },
                     },
-                },
-                raw: true,
+                    raw: true,
+                });
+
+                if (session && session.id) {
+                    req.session_id = decodedToken.session_id;
+                    req.user_id = decodedToken.user_id;
+
+                    next();
+                } else {
+                    next();
+                }
             });
+        } else {
+            next()
+        }
 
-            if (session && session.id) {
-                req.session_id = decodedToken.session_id;
-                req.user_id = decodedToken.user_id;
 
-                next();
-            } else {
-                next();
-            }
-        });
     } catch (error) {
         console.log({ error })
         next();
